08105ff186f64ac5a46622f759b1abd152c2a527
User <beckham051188@gmail.com>
2022-03-16 13:55:42 +0800

    update

--- 1.py
+++ 1.py
@@ -1,5 +1,2 @@
-import keyword
-print(keyword.kwlist)
-print(keyword.iskeyword('if'))
-print(keyword.iskeyword('IF'))
-print(str(10)+10)
+
+input(["機掰"])
--- hw0.py
+++ hw0.py
@@ -1,4 +1,8 @@
 def hw0_0():
     n10 = 1
-    for
+    for i in range(10):
+        n10 *= (i+1)
+    print(n10)
     return n10
+
+hw0_0()
